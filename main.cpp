f31a708a595e2f0351fea693d88157f736634af0
Marat <62611082+maratx86@users.noreply.github.com>
2022-10-19 01:30:04 +0300

    some new tests

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
     std::cout << "- (1, 2) -> " << root->segment_value(1, 2) << std::endl;
     std::cout << "- (1, 3) -> " << root->segment_value(1, 3) << std::endl;
     std::cout << "- (2, 3) -> " << root->segment_value(2, 3) << std::endl;
-    std::cout << "- (3, 4) -> " << root->segment_value(1,3) << std::endl;
+    std::cout << "- (3, 4) -> " << root->segment_value(3,4) << std::endl;
     delete root;
 
     std::cout << std::endl;
@@ -37,6 +37,24 @@ int main() {
     std::cout << "- (1, 3) -> " << root->segment_value(1, 3) << std::endl;
     std::cout << "- (2, 3) -> " << root->segment_value(2, 3) << std::endl;
     std::cout << "- (3, 4) -> " << root->segment_value(3, 4) << std::endl;
+    delete root;
+
+    std::cout << std::endl;
+    root = new Root(min, std::vector<int> {5, 3, 0, 6, 2, 4, 9, 5});
+    std::cout << "# Root(min, {5, 3, 0, 6, 2, 4, 9, 5})" << std::endl;
+    std::cout << "- (1, 4) -> " << root->segment_value(1, 4) << std::endl;
+    std::cout << "- (1, 2) -> " << root->segment_value(1, 2) << std::endl;
+    std::cout << "- (1, 3) -> " << root->segment_value(1, 3) << std::endl;
+    std::cout << "- (2, 3) -> " << root->segment_value(2, 3) << std::endl;
+    std::cout << "- (3, 4) -> " << root->segment_value(3,4) << std::endl;
+    std::cout << std::endl;
+    std::cout << "- (4, 6) -> " << root->segment_value(4, 6) << std::endl;
+    std::cout << "- (4, 5) -> " << root->segment_value(4, 5) << std::endl;
+    std::cout << "- (3, 5) -> " << root->segment_value(3, 5) << std::endl;
+    std::cout << "- (5, 8) -> " << root->segment_value(5, 8) << std::endl;
+    std::cout << "- (6, 8) -> " << root->segment_value(6, 8) << std::endl;
+    std::cout << "- (7, 8) -> " << root->segment_value(7, 8) << std::endl;
+    std::cout << "- (4, 8) -> " << root->segment_value(4, 8) << std::endl;
     delete root;
     return 0;
 }
